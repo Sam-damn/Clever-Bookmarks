61450a867078b26aa66ab1fa2a427c784fb6ff49
Sameh <samxxhaider@gmail.com>
2023-07-14 02:18:19 +0300

    Implement API for creating a new bookmark

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -9,15 +9,25 @@ def _get_bookmark(url):
     bookmark = Bookmark.objects.get(url=url)
     return bookmark
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def bookmarks_list(request):
     """
-         API Controller to list all avaiable bookmarks or create a new one
+         API EndPoint for listing all avaiable bookmarks or creating a new Bookmark
     
     """
-    bookmarks = Bookmark.objects.all()
-    bookmarkSerializer = BookmarkSerializer(bookmarks, many=True)
-    return Response(bookmarkSerializer.data)
+
+    if request.method == "GET":
+
+        bookmarks = Bookmark.objects.all()
+        bookmarkSerializer = BookmarkSerializer(bookmarks, many=True)
+        return Response(bookmarkSerializer.data)
+    
+    elif request.method == "POST": 
+        serializer = BookmarkSerializer(data=request.query_params)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
@@ -38,6 +48,8 @@ def bookmark_controller(request, url):
     elif request.method == 'PUT' : 
         serializer = BookmarkSerializer(bookmark)
         serializer.update(bookmark, request.query_params)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.data)
     
     elif request.method == 'DELETE':
